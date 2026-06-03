bf27381a2d25434738da12e5eb73d35c9f8069fa
Pavel Kovalchuk <pavelkovalchuk0510@gmail.com>
2026-06-03 20:27:05 +0200

    responsive home hero image

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,8 +1,10 @@
 import DropListSection from "@/components/home/DropListSection";
+import HomeHeroDesktop from "@/components/home/HomeHero";
 
 export default function Home() {
   return (
     <>
+      <HomeHeroDesktop />
       <DropListSection />
     </>
   );
